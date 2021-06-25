49a32307a996223cb1859a751d2f05dc0b85ddff
simensgreen <simensgreen007@gmail.com>
2021-06-26 00:30:34 +0300

    Таблица истории

--- gui/docks/_history_dock.py
+++ gui/docks/_history_dock.py
@@ -1,4 +1,4 @@
-from PyQt5.QtWidgets import QWidget
+from PyQt5.QtWidgets import QTableWidget, QTableWidgetItem
 from pyqtgraph.dockarea import Dock
 
 from utils import AppData, AppEvent
@@ -7,10 +7,18 @@ from utils import AppData, AppEvent
 class HistoryDock(Dock):
     def __init__(self, app_data: AppData):
         super().__init__("История операций")
-        self.widget = QWidget()
-        self.addWidget(self.widget)
         self.app_data = app_data
+        self.widget = QTableWidget()
+        self.widget.setColumnCount(3)
+        self.widget.setRowCount(100)
+        self.addWidget(self.widget)
         self.handler_id = app_data.handlers.add(self.update, AppEvent.HistoryChanged)
 
     def update(self):
-        pass
+        self.widget.clear()
+
+        self.widget.setHorizontalHeaderLabels(("Время", "Команда", "Статус"))
+        for no, event in enumerate(self.app_data.history):
+            self.widget.setItem(no, 0, QTableWidgetItem(str(event.date)))
+            self.widget.setItem(no, 1, QTableWidgetItem(str(event.command)))
+            self.widget.setItem(no, 2, QTableWidgetItem(str(event.status)))
